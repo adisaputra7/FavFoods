2577919ebff5153c61d23c425d165e914c1c1d5c
Nurhadi Saputra <nurhadisaputra7@gmail.com>
2023-06-09 15:00:56 +0700

    UI Onboarding

---
+++ FavFoods/GetStartedView.swift
@@ -0,0 +1,46 @@
+//
+//  GetStartedView.swift
+//  FavFoods
+//
+//  Created by Nur Hadi Saputra on 09/06/23.
+//
+
+import SwiftUI
+
+struct GetStartedView: View {
+    var body: some View {
+        VStack(spacing: 0.0){
+            Image("onboarding_illustration")
+                .padding(.bottom, 50.0)
+            HStack{
+                VStack(alignment: .leading, spacing: 0.0){
+                    Text("180K Store")
+                        .font(.custom("Poppins-Reguler", size: 16))
+                        .foregroundColor(Color("Grey"))
+                    Text("Order Your\nFavorite Foods")
+                        .font(.custom("Poppins-Bold", size: 36))
+                        .foregroundColor(Color("Navy"))
+                    Button(action: {
+                        print("Get Started")
+                    }, label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 50)
+                                .frame(width: 323, height: 50)
+                                .foregroundColor(Color("Orange"))
+                            Text("Explore Now")
+                                .foregroundColor(Color("White"))
+                                .font(.custom("Poppins-SemiBold", size: 16))
+                        }
+                    })
+                    .padding(.top, 30.0)
+                }
+            }
+        }
+    }
+}
+
+struct GetStartedView_Previews: PreviewProvider {
+    static var previews: some View {
+        GetStartedView()
+    }
+}
