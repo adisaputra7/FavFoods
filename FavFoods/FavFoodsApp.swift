4fabcefe8dd30717277b29568b1db94f83bcc474
Nurhadi Saputra <nurhadisaputra7@gmail.com>
2023-06-09 08:43:04 +0700

    Initial Commit

---
+++ FavFoods/FavFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsApp.swift
+//  FavFoods
+//
+//  Created by Nur Hadi Saputra on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
